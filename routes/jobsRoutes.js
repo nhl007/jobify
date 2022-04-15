48087bdb24da3d6008f1b17d02d3e4020e71de34
nhl007 <apar.asif.an@gmail.com>
2022-04-16 01:14:29 +0600

    april 15th

---
+++ routes/jobsRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router=express.Router()
+
+import { createJob,deleteJob, getAllJobs, updateJob, showStats } from "../controllers/jobsControler.js";
+
+router.route('/').post(createJob).get(getAllJobs)
+// place before :id
+router.route('/stats').get(showStats)
+router.route('/:id').delete(deleteJob).patch(updateJob)
+
+
+
+
+export default router 
--- server.js
+++ server.js
@@ -4,17 +4,36 @@ const app=express()
 import dotenv from'dotenv'
 dotenv.config()
 
+//port
+const port=process.env.PORT || 5000
 
-
+//db
 import ConnectDb from "./db/connect.js";
+
+//auth routes
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobsRoutes.js'
 
 //not found middleware 
 import notFoundMiddleware from "./middleware/not-found.js";
 //error handler middleware
 import errorHandlerMiddleware from'./middleware/error-handler.js'
-const port=process.env.PORT || 5000
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Welcome')
+})
+
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/jobs',jobsRouter)
+
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
+
+
+
 
 const Start=async()=>{
     try {
